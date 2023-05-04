c36ccf19963f6f2ff40f6ff27b13dc59518ea094
t1m0nya0 <t.nurbulatuly@gmail.com>
2023-05-05 04:05:38 +0600

    add permission, update views + urls

--- basta/courses/urls.py
+++ basta/courses/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
-
 from .views import *
 
 
-router_courses = routers.SimpleRouter()
-router_courses.register(r'courses', CourseViewSet)
 router_categories = routers.SimpleRouter()
 router_categories.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
     path('category/<int:cat_id>/courses/', CourseByCategoryList.as_view()),
-    path('', include(router_courses.urls)),
+    path('courses/', CourseApiList.as_view()),
+    path('courses/<int:pk>/', CourseApiDetail.as_view()),
     path('', include(router_categories.urls)),
 ]
